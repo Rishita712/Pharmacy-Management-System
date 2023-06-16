b957d683de80dd10a26e62b2447cc93b7c84f8a8
Rishita712 <97270530+Rishita712@users.noreply.github.com>
2023-06-16 11:53:27 +0530

    Add files via upload

---
+++ rishita1.php
@@ -0,0 +1,204 @@
+<?php
+
+include('db.php');
+
+?>
+
+
+
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pharmacy Management System</title>
+
+    
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="rishita1.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo"> <i class="fas fa-heartbeat"></i> Pharmacy </a>
+
+    <nav class="navbar">
+        <a href="#">home</a>
+        <a href="our services.html">services</a>
+        <a href="about us.html">about</a>
+        <a href="log.html">Register/Log-in</a>
+        <a href="review.html">review</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="image">
+        <img src="pms final img.png" alt="">
+    </div>
+
+    <div class="content">
+        <h3>stay safe, stay healthy</h3>
+        <p>The pharmacy Management System is built in order to replace manual based system to computerize.</p>
+        <p>Here System is expected to be efficient useful and affordable on implementing task that is order by the Pharmacy Manager.</p>
+        <a href="#" class="btn"> contact us <span class="fas fa-chevron-right"></span> </a>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- icons section starts  -->
+
+
+
+
+
+
+
+
+<!-- about section ends -->
+
+<!-- doctors section starts  -->
+
+<!-- doctors section ends -->
+
+<!-- booking section starts   -->
+
+
+<section class="blogs" id="blogs">
+
+    <h1 class="heading"> our <span>blogs</span> </h1>
+
+    <div class="box-container">
+
+        
+
+        <div class="box">
+            <div class="image">
+                <img src="ved.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    
+                </div>
+                <h3>Vedant jain</h3>
+                <p>Vedant Jain from Poornima Institute of Engineering and Technology Jaipur</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="mpf.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                   
+                
+                </div>
+                <h3>           Rishita maheshwari</h3>
+                <p>Rishita Maheshwari from Poornima Institute of Engineering and Technology Jaipur</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="vedik.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    
+                </div>
+                <h3>vaidik saraswat</h3>
+                <p>Vaidik Saraswat from Poornima Institute of Engineering and Technology Jaipur</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blogs section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i> home </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> services </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> about </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> doctors </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> book </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> review </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> blogs </a>
+        </div>
+
+        <div class="box">
+            <h3>our services</h3>
+            <a href="#"> <i class="fas fa-chevron-right"></i> dental care </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> message therapy </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> cardioloty </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> diagnosis </a>
+            <a href="#"> <i class="fas fa-chevron-right"></i> ambulance service </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +919214467008</a>
+            <a href="#"> <i class="fas fa-phone"></i> +917891095395 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> 2021pietadvedant056@poornima.org </a>
+            <a href="#"> <i class="fas fa-envelope"></i> 2021pietadrishita042@poornima.org </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> PIET jaipur, sitapura 302022 </a>
+        </div>
+
+        
+
+    </div>
+
+    <div class="credit"> created by <span>vedant jain & Rishita maheshwari </span>  </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
